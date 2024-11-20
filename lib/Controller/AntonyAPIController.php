199175ffdd60d49a822ea379aa522732ffd2e739
Martin Linkwitz <martin@die-groupware.de>
2024-11-20 14:42:34 +0100

    changed api to create a folder with the $prefix in the users root and move the share below it.

--- lib/Controller/AntonyAPIController.php
+++ lib/Controller/AntonyAPIController.php
@@ -8,75 +8,61 @@ use OCP\AppFramework\OCSController;
 use OCP\IRequest;
 use Throwable;
 use OCP\Share\IManager as ShareManager;
+use OCP\Files\IRootFolder;
 
 class AntonyAPIController extends OCSController {
 
-	public function __construct(
-		string             $appName,
-		IRequest           $request,
-		protected ShareManager $shareManager,
-		private ?string    $userId
-	) {
-		parent::__construct($appName, $request);
-	}
+        public function __construct(
+                string             $appName,
+                IRequest           $request,
+                protected ShareManager $shareManager,
+                protected IRootFolder $rootFolder,
+                private ?string    $userId
+        ) {
+                parent::__construct($appName, $request);
+        }
 
-	/**
-	 * @NoAdminRequired
-	 *
-	 * @param string@null $fullShareId
-	 * @param string|null $prefix
-	 * @return DataResponse
-	 */
-	public function renameShareFolder(?string $fullShareId, ?string $prefix = null): DataResponse {
-		if ($fullShareId == null || $prefix == null)
-			return new DataResponse(['error' => "Parameters missing"], Http::STATUS_BAD_REQUEST);
-		
-		try {
-			$share = $this->shareManager->getShareById($fullShareId);
-			
-			if (!$this->canRenameShare($share)) {
-				return new DataResponse(['error' => "Only users with permission can rename shares"], Http::STATUS_BAD_REQUEST);
-			}			
-			
-			$currentPath = $share->getTarget();
-			$newPath = "/" . $prefix . " " . trim($currentPath, "/");			
-			
-			$share->setTarget($newPath);
-			$share = $this->shareManager->moveShare($share, $share->getSharedWith());
-			
-			return new DataResponse(['success' => true, 'newPath' => $newPath], Http::STATUS_OK);
-		
-		} catch (Exception | Throwable $e) {		
-			return new DataResponse(['error' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
-		}
-	}
-	
-	/**
-	 * Does the user have permission the rename the share
-	 *
-	 * @param \OCP\Share\IShare $share the share to check
-	 * @return boolean
-	 */
-	protected function canRenameShare(\OCP\Share\IShare $share): bool {
-		// A file with permissions 0 can't be accessed by us. So Don't show it
-		if ($share->getPermissions() === 0) {
-			return false;
-		}
+        /**
+         * @NoAdminRequired
+         *
+         * @param string@null $fullShareId
+         * @param string|null $prefix
+         * @return DataResponse
+         */
+        public function renameShareFolder(?string $fullShareId, ?string $prefix = null): DataResponse {
+                if ($fullShareId == null || $prefix == null)
+                        return new DataResponse(['error' => "Parameters missing"], Http::STATUS_BAD_REQUEST);
 
-		// if the user is the recipient, i can rename
-		if ($share->getShareType() === \OCP\Share\IShare::TYPE_USER && $share->getSharedWith() === $this->userId)
-		{			
-			return true;
-		}
+                try {
+                        $share = $this->shareManager->getShareById($fullShareId);
+                        $user = $share->getSharedWith();
 
-		// The owner of the file and the creator of the share can always rename
-		if ($share->getShareOwner() === $this->userId || $share->getSharedBy() === $this->userId) 
-		{			
-			return true;
-		}
+                        $usersRootFolder = $this->rootFolder->getUserFolder($user);
+                        try
+                        {
+                                $usersRootFolder->get($prefix);
+                        } catch (Exception | Throwable $e)
+                        {
+                                $usersRootFolder->newFolder($prefix);
+                        }
 
-		return false;
-	}	
+                        $currentPath = $share->getTarget();
+                        $newPath = "/" . $prefix . "/" . trim($share->getNode()->getName(), "/");
+
+                        $share->setTarget($newPath);
+                        $share = $this->shareManager->moveShare($share, $share->getSharedWith());
+
+                        return new DataResponse([
+                                'success' => true,
+                                'newPath' => $newPath,
+                                'user' => $user
+                        ], Http::STATUS_OK);
+
+                } catch (Exception | Throwable $e) {
+                        return new DataResponse(['error' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
+                }
+        }
 
 
 }
+?>
